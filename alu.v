0563b6fee68c78df2ad1cef8d5bc4812d0b64d6c
Sergio <sergioandyp@gmail.com>
2023-05-16 00:21:43 -0300

    Operaciones de ALU y Decoder (faltan) Agregado operacion de suma

--- alu.v
+++ alu.v
@@ -1,20 +1,20 @@
-`define ALU_ADD		10'bx0xxxxx000
+`define ALU_ADD		10'bx0xxxxx000		// Mismas operaciones con o sin inmediato
 `define ALU_SUB		10'bx1xxxxx000
 `define ALU_SLL		10'b0000000001      // Logic Shift Left
 `define ALU_SRL		10'b0000000101      // Logic Shift Right
 `define ALU_SRA		10'b0100000101      // Arithmetic shift right
 `define ALU_XOR		10'b0000000100
-`define ALU_OR			10'b0000000110
+`define ALU_OR		10'b0000000110
 `define ALU_AND		10'b0000000111
 `define ALU_SLT		10'b0000000010      // Set less than
-`define ALU_SLTU		10'b0000000011      // Set less than unsigned
+`define ALU_SLTU	10'b0000000011      // Set less than unsigned
 
 
 module alu
 (
 	input[31:0] busA,
 	input[31:0] busB,
-	input[31:0] imm,		// Inmediate
+	input[31:0] imm,		// Immediate
 	input imm_en,
 	input[9:0] ctrl,
 	output reg[31:0] out,
@@ -34,11 +34,11 @@ begin
 			`ALU_SRL:	out = busA >> (busB[4:0]);
 			`ALU_SRA:	out = $signed(busA) >>> (busB[4:0]);
 			`ALU_XOR:	out = busA ^ busB;
-			`ALU_OR:		out = busA | busB;
+			`ALU_OR:	out = busA | busB;
 			`ALU_AND:	out = busA & busB;
 			`ALU_SLT:	out = (busA < busB) ? 1 : 0;
 			`ALU_SLTU:	out = (busA < busB) ? 1 : 0;
-			default: out=32'hABCD;
+			default: out=0;
 		endcase
 	end
 	else begin
@@ -49,11 +49,11 @@ begin
 			`ALU_SRL:	out = busA >> (imm[4:0]);
 			`ALU_SRA:	out = $signed(busA) >>> (imm[4:0]);
 			`ALU_XOR:	out = busA ^ imm;
-			`ALU_OR:		out = busA | imm;
+			`ALU_OR:	out = busA | imm;
 			`ALU_AND:	out = busA & imm;
 			`ALU_SLT:	out = (busA < imm) ? 1 : 0;
 			`ALU_SLTU:	out = (busA < imm) ? 1 : 0;
-			default: out=32'hABCD;
+			default: out=0;
 		endcase 
 	end
 end
--- decoder.v
+++ decoder.v
@@ -1,33 +1,21 @@
-// `define ALU_ZERO	4'b0000     // 0x0000
-// `define ALU_ONE		4'b0001
-// `define ALU_A		4'b0010 
-// `define ALU_B		4'b0011
-// `define ALU_INC		4'b0100
-// `define ALU_DEC		4'b0101
-// `define ALU_ADD		4'b1000     // A + B
-// `define ALU_SUB		4'b1001		// A - B
-// `define ALU_SHL		4'b1010
-// `define ALU_SHR		4'b1011
-
-`define LUI_U_Type      7'b0110111   
+`define R_Type          7'b0110011
+`define IMM_Type       	7'b0010011	// Incluye I-type y shifts IMM
+`define B_Type          7'b1100011
+`define S_Type          7'b0100011
+`define LUI_U_Type      7'b0110111
 `define AUIPC_U_Type    7'b0010111   
 `define J_Type          7'b1101111
-`define I_Type          7'b1100111  // JALR
-`define B_Type          7'b1100011
+`define JALR_I_Type     7'b1100111  // JALR
 `define L_Type          7'b0000011
-`define S_Type          7'b0100011  
-//`define SH_R_Type       7'b0010011
-`define IMM_Type       	7'b0010011    
-`define R_Type          7'b0110011
 
 
 
 module decoder(
 	input[31:0] instruction,
 
-	output wire [9:0] aluCtrl,   // funct7 + funct3
+	output wire [9:0] aluCtrl,   // {funct7, funct3}
 	output wire [31:0] imm,
-	output wire [5:0] selA,      // Este tiene 5 porque incluye al PC
+	output wire [5:0] selA,      // Este tiene 6 porque incluye al PC
 	output wire [4:0] selB,
 	output wire [5:0] selOut,
     output wire imm_en
@@ -39,7 +27,7 @@ module decoder(
 	reg [5:0] curr_selA;      
 	reg [4:0] curr_selB;
 	reg [5:0] curr_selOut;
-   reg curr_imm_en;
+    reg curr_imm_en;
 
 	always @(instruction) begin
         curr_selOut = 0;
@@ -57,43 +45,51 @@ module decoder(
             end
             
             `IMM_Type: begin
-                curr_selA = instruction[19:15]; curr_imm = instruction[31:20]; curr_selOut = instruction[11:7];
+                curr_selA = instruction[19:15]; curr_selOut = instruction[11:7];
+                // Extension de signo para valores inmediatos
+                curr_imm[31:11] = {21{instruction[31]}}; curr_imm[10:0] = instruction[30:20];
+                // Para facilitar la logica, siempre ponemos todo el inmediato y el aluCtrl, despues la ALU se encarga
                 curr_aluCtrl = { instruction[31:25], instruction[14:12] };
-                curr_imm_en = 1; 
-            end
-
-            `I_Type: begin  //Ver, es media turbia
-                curr_selA = instruction[19:15]; curr_imm = instruction[31:20]; curr_selOut = instruction[11:7]; 
-                curr_aluCtrl = instruction [14:12];   // JALR (Jump And Link Register) salto a subrutina
-                curr_imm_en = 1;
-            end  
-
-            `S_Type: begin
-                curr_selA = instruction[19:15]; curr_selB = instruction[24:20]; // ESTO VA A MEMORIA, HAY QUE AVISAR DE ALGUNA FORMA 
-                curr_imm = { instruction[31:25], instruction[14:12] }; //curr_aluCtrl = OPCODE_ALU_SUMA_IMM 
                 curr_imm_en = 1;
             end
-
-            `B_Type: begin
-                curr_selA = instruction[19:15]; curr_selB = instruction[24:20];
-                curr_imm[31:12] = instruction[31];
-                curr_imm[11:0] = { instruction[7], instruction[30:25], instruction [11:8], 1'b0}; //curr_aluCtrl = OPCODE_ALU_RESTA;
-					//curr_jmp_ctrl = [];
-                curr_imm_en = 1; 
-            end
-          
-            `LUI_U_Type: begin
-                curr_imm = instruction[31:12]; curr_selOut = instruction[11:7];
-                curr_imm_en = 1;
-            end
-
-            `AUIPC_U_Type: begin
-                curr_imm = instruction[31:12]; curr_selOut = instruction[11:7];
-                curr_imm_en = 1;
-            end
+            
+            // `B_Type: begin
+            //     curr_selA = instruction[19:15]; curr_selB = instruction[24:20];
+            //     curr_imm[31:12] = {20{instruction[31]}};
+            //     curr_imm[11:0] = { instruction[7], instruction[30:25], instruction [11:8], 1'b0};
+            //     // TODO: Ver como hacerlo
+            //         // Una forma: Hacer siempre la resta y pasarle el fun3 al JMP Control y que se fije
+            //     //curr_aluCtrl = OPCODE_ALU_RESTA;
+            //     curr_imm_en = 0;
+            //     // TODO: Avisar al JMP Control
+            //     //curr_jmp_ctrl = [];
+            // end
 				
-				default:
-					curr_selOut= 0;
+            // `JALR_I_Type: begin  //Ver, es media turbia
+            //     curr_selA = instruction[19:15]; curr_imm = instruction[31:20]; curr_selOut = instruction[11:7]; 
+            //     curr_aluCtrl = instruction [14:12];   // JALR (Jump And Link Register) salto a subrutina
+            //     curr_imm_en = 1;
+            //     // TODO: Debe setear el ultimo bit en 0. Escribe PC+4 en rd
+            // end
+
+            // `S_Type: begin
+            //     curr_selA = instruction[19:15]; curr_selB = instruction[24:20]; // ESTO VA A MEMORIA, HAY QUE AVISAR DE ALGUNA FORMA 
+            //     curr_imm = { instruction[31:25], instruction[14:12] }; //curr_aluCtrl = OPCODE_ALU_SUMA_IMM 
+            //     curr_imm_en = 1;
+            // end
+          
+            // `LUI_U_Type: begin
+            //     curr_imm = instruction[31:12]; curr_selOut = instruction[11:7];
+            //     curr_imm_en = 1;
+            // end
+
+            // `AUIPC_U_Type: begin
+            //     curr_imm = instruction[31:12]; curr_selOut = instruction[11:7];
+            //     curr_imm_en = 1;
+            // end
+				
+            // default:
+            //     curr_selOut = 0;
         endcase
 	end
 
